cf9cc6cc3fa6325c2f2ef10fd1b6ce65524abf75
mfordas <54579900+mfordas@users.noreply.github.com>
2022-11-01 20:01:29 +0100

    feat: Upgrade packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const path = require("path");
-const glob = require("glob");
+const path = require('path');
+const glob = require('glob');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
@@ -12,56 +12,54 @@ module.exports = {
     path: path.resolve(__dirname + '/Dist'),
     filename: 'index.js',
   },
-    devtool: "source-map",
-    resolve: {
-      extensions: ['.js'],
-    },
-    devServer: {
-      index: 'Wthr.html',
-      contentBase: path.join(__dirname, 'dist'),
-      compress: true,
-      port: 8080
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js)$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-              },
-              {
-                test: /\.(png|jpe?g|gif)$/i,
-                loader: 'file-loader',
-                options: {
-                    outputPath: './img',
-                  },
-              },
-              {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                  fallback: 'style-loader',
-                  use: 'css-loader'
-                }),
-
-              }
-            ]
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        filename: 'Wthr.html',
-        template: './src/html/index.html'
-      }),
-      new ExtractTextPlugin({
-        filename: 'index.css'
-      }),
-      new OptimizeCssAssetsPlugin({
-        assetNameRegExp: /\.css$/g,
-        cssProcessor: require('cssnano'),
-        cssProcessorOptions: {discardComments: {removeAll: true}},
-        canPrint: true
-      }),
-      new PurifyCSSPlugin({
-        paths: glob.sync(path.resolve(__dirname, 'src/html/*.html')),
-      })
-    ]
-}
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js'],
+  },
+  devServer: {
+    index: 'Wthr.html',
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          outputPath: './img',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader',
+        }),
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/html/index.html',
+    }),
+    new ExtractTextPlugin({
+      filename: 'index.css',
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorOptions: { discardComments: { removeAll: true } },
+      canPrint: true,
+    }),
+    new PurifyCSSPlugin({
+      paths: glob.sync(path.resolve(__dirname, 'src/html/*.html')),
+    }),
+  ],
+};
